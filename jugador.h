f33dadb998053a3874c5ada01210ff231aa97fe7
Toby-Leanza <leanzatobias@outlook.com>
2024-10-15 14:46:46 -0300

    Update jugador.h

--- jugador.h
+++ jugador.h
@@ -1,9 +1,6 @@
 #ifndef JUGADOR_H
 #define JUGADOR_H
 
-// Función que elige una palabra de la base de datos según la frecuencia
-char* elegirPalabra(char palabras[][6], int frecuencias[], int numPalabras);
-
 // Función que filtra palabras según las reglas del juego
 void filtrarPalabras(char palabras[][6], int frecuencias[], int* numPalabras, char* intento, char resultado[5]);
 
